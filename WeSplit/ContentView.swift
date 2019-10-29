f1c9145f87a402518e9d3281a336f3584cab50be
alindnbrg <mail@andrelindenberg.de>
2019-10-29 08:50:30 +0100

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by André Lindenberg on 29.10.19.
+//  Copyright © 2019 André Lindenberg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
